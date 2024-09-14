b0481a281ef44a49c4668299ffd58ca39262ad55
NikChaPhoto <apbnikita@gmail.com>
2024-09-14 20:33:50 +0200

    One Step

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Vielen Dank für Ihre Nachricht!');
+    // Hier können Sie zusätzliche Logik für das Formular hinzufügen
+});
